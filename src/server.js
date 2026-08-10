db5667dd69d5ab026ed88257d00b452f94fbab5f
cesarmanzano1 <131565218+cesarmanzano1@users.noreply.github.com>
2026-08-09 23:17:53 -0300

    Agrego correcciones para conectarse a la DB

--- src/server.js
+++ src/server.js
@@ -1,20 +1,18 @@
-/*  src/server.js */
-
-const express = require("express")
-const swaggerUi = require("swagger-ui-express")
-const { swaggerSpec } = require("./swagger")
-const { router } = require("./router/router")
-const { requestLogger } = require("./middleware/middleware")
-
 const app = express()
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "API funcionando correctamente"
+    })
+})
 
 app.use(requestLogger)
 
 app.use(express.json())
 
 app.get("/api-docs.json", (req, res) => {
-  res.setHeader("Content-Type", "application/json")
-  res.send(swaggerSpec)
+    res.setHeader("Content-Type", "application/json")
+    res.send(swaggerSpec)
 })
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
@@ -22,5 +20,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(router)
 
 module.exports = {
-  app
+    app
 }
